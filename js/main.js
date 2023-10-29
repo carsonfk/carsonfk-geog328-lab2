a1dbf10104b69efe4bebe4d357eb5cefbd45a902
carsonfk <carson.f.k@outlook.com>
2023-10-29 15:44:18 -0700

    update html+css+js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let icon = document.getElementsByClassName("icon")[0];
 
-icon.addEventListener('click', responsive_control);
+//icon.addEventListener('click', responsive_control);
 
 function responsive_control() {
   let x = document.getElementById("myTopnav");
@@ -9,4 +9,23 @@ function responsive_control() {
   } else {
     x.className = "topnav";
   }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
